fb6bfc939c63f805c64ac7d05269268b72759cf8
Adrian Petria <141153198+LeggyLadd@users.noreply.github.com>
2023-11-29 13:01:42 -0500

    Flappy Tarnished

---
+++ assignment14/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1701277793,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/0ff74570cb6b2cdea9c137256e8278bc-sheet0.png",
+		"images/flappytarnished-sheet0.png",
+		"images/start-sheet0.png",
+		"images/tarnished-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/eldenfloor.png",
+		"images/youdied-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/home-sheet0.png",
+		"images/retry-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
